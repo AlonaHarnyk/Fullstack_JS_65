a2a904e14a16406527bde4f772efa677f8e32955
Alona Harnyk <alyonagarnyk@gmail.com>
2022-11-02 21:11:55 +0200

    02.11

--- script.js
+++ script.js
@@ -1,156 +1,177 @@
-// const a = [1, 2, 3, 4, 5]
-// for(let i = 0; i < a.length; i+=1) {
-//     console.log(a[i] + 10)
-// }
-// for(const elem of a) {
-//     console.log(elem + 10)
+
+// function isIncluded(array, number) {
+//     const result = array.includes(number)
+//     console.log(result)
 // }
 
-// const a = 2;
-// const b = a;
-// console.log(a === b)
+// isIncluded([1, 2, 5, 8, 3], 4) // false
+// isIncluded([1, 2, 5, 8, 3], 5) // true
+// isIncluded(5, [1, 3, 5]) // error
 
-// const array1 = [1, 2, 3];
-// const array2 = array1;
 
-// console.log(array1)
-// console.log(array2)
-// console.log(array1 === array2)
-
-// array1[0] = 10;
-
-// console.log(array1)
-// console.log(array2);
-
-// array2[2] = 5;
-
-// console.log(array1)
-
-// const array3 = array2
-
-// console.log(array3)
-
-// array3[1] = 100;
-
-// console.log(array1)
-
-// array2.length = 0;
-
-// console.log(array3)
-
-// array1[2] = 1000;
-// console.log(array3)
-
-// for (const elem of array3) {
-//     console.log(elem)
+// function isIncluded(array, number) {
+//     const result = array.includes(number)
+//     console.log(result)
+//     // return result
+//     // console.log(result)
 // }
-// console.log(array3.length)
+
+// const a = isIncluded([1, 2, 5, 8, 3], 4)
+// console.log(a)
+
+// console.log(isIncluded([1, 2, 5, 8, 3], 4)) // false
+// console.log(isIncluded([1, 2, 5, 8, 3], 5)) // true
+
+// const a = isIncluded([1, 2, 5, 8, 3], 4)
+// console.log(a)
+// const b = isIncluded([1, 2, 5, 8, 3], 5)
+// console.log(b)
+
+// function add(a, b) {
+//   // const sum = a + b
+//   // return sum
+
+//   return a + b;
+// }
+
+// const number1 = add(1, 3);
+// const number2 = add(5, 7);
+
+// const sum = number1 + number2
+
+// const sum = add(1, 3) + add(5, 7)
+// console.log(sum)
+
+// function add(a, b) {
+//     if (typeof a !== 'number' || typeof b !== 'number') {
+//         console.log('One of operands is not a number')
+//         return
+//         // return 'One of operands is not a number'
+//     }
+//     return a + b
+// }
+
+// // console.log(add(3, 7))
+// console.log(add( 5, '10'))
 
 
-// let array1 = [1, 3, 5]
-// let array2 = array1;
+// function add(a = 0, b = 0) {
+//   // const sum = a + b
+//   // return sum
+//  console.log(a)
+//  console.log(b)
+//  return a + b;
+// }
 
-// array1[0] = 50;
-// console.log(array2)
+// console.log(add(1))
 
-// array1 = [9, 90, 900]
+// function add(a = 0, b = 0, c = 0) {
+//     console.log(a + b + c)
+// }
 
-// console.log(array1)
-// console.log(array2)
+// const num1 = 3;
+// const num2 = 4
 
-// array2 = array1;
+// // add(num1, num2)
+// // add(3)
+// add()
 
-// console.log(array1)
-// console.log(array2)
+// function add() {
+//     console.log(arguments)
+//     console.log(arguments[1])
+//     console.log(arguments.length)
+//     let sum = 0;
+//     for(const arg of arguments) {
+//         // console.log(arg)
+//         sum +=arg
+//     }
+//     // console.log(sum)
+//     return sum
+// }
 
-// const a = [1, 2, 3]
-// const b = a
-// console.log(a === b) // true
-
-// const c = [1, 2, 3]
-// const d = [1, 2, 3]
-// console.log(c === d) // false
-// console.log(c[0] === d[0]) // true
-
-// const e = [1, 2, [3, 4]]
-// // const f = [1, 2, [3, 4]]
-// // console.log(e[2] === f[2]) // false
-// console.log(e[2][0]) // 3
-
-
-// const string = '1-!2-!3-!4-!5-!6-!7-!8-!9'
-
-// console.log(string.split('!'))
-
-// const string2 = '123456789'
-
-// const array = string2.split('')
-
-// console.log(array)
+// add(5, 7, 4, 9, 10)
+// console.log(add(5, 7, 4, 9, 10))
 
 
-// const array3 = [1, 2, 3, 4, 5, 6, 7];
-// const string3 = array3.join('***')
-// console.log(string3)
+// function isIncluded() {
+//     console.log(arguments)
+//     const array = Array.from(arguments);
+//     console.log(array)
+//     const number = arguments[arguments.length - 1]
+//     console.log(number)
+//     const array2 = array.slice(0, -1)
+//     console.log(array2)
+//     console.log(array2.includes(number))
+//     return array2.includes(number)
+// }
+
+// function isIncluded(...array) {
+//     console.log(array)
+//     // const number = arguments[arguments.length - 1]
+//     // console.log(number)
+//     // const array2 = array.slice(0, -1)
+//     // console.log(array2)
+//     // console.log(array2.includes(number))
+//     // return array2.includes(number)
+// }
+
+// isIncluded(2, 5, 6, 8, 4, 9, 3, 7)
+
+// const add = function(a, b) {
+//     if(typeof a !== 'number' || typeof b !== 'number') {
+//         return 'One of operands is not a number'
+//         // return
+//     }
+//     return a + b
+// }
+
+// console.log(add(3, 7))
+// console.log(add( 5, '10'))
 
 
-// const array5 = [1,4,6,7,4,7,89]
-// // const res1 = array5.includes(100 && 4)
-// // console.log(res1) // true
-// const res2 = array5.includes(100) && array5.includes(4)
-// console.log(res2) // false
 
-// const emptyArray = [0, 90]
-// const length = emptyArray.push(1, 2, 3, 100, 200, 1000, 100000)
-// console.log(emptyArray)
-// console.log(length)
+// const a = 1;
+// console.log(a);
+// function logger() {
+//   const a = 2;
+//   // console.log(a)
+//   if (a === 2) {
+//     // const a = 3;
+//     // console.log(a)
+//     for (let i = 0; i <= 10; i += 1) {
+//       //   const a = 4;
+//       console.log(a);
+//       // console.log(a + i)
+//     }
+//   }
+// }
 
-// const notEmptyArray = [1, 2, 3, 4, 5]
-// const deletedEl = notEmptyArray.pop()
-// console.log(notEmptyArray)
-// console.log(deletedEl)
-
-// const superArray = [1, 2, 3, 4, 5]
-// const length1 = superArray.unshift(5, 10)
-// console.log(superArray) 
-// console.log(length1)
-
-// const superArray1 = [1, 2, 3, 4, 5]
-// const deletedEl1 = superArray1.shift()
-// console.log(superArray1)
-// console.log(deletedEl1)
-
-// const array6 = [1, 2, 3, 4, 5, 6, 7]
-// const array7 = array6.slice(0, 5)
-// // console.log(array7)
-// const array8 = array6.slice(-2)
-// console.log(array8)
-// console.log(array6)
+// logger();
 
 
-// const numbers2 = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9]
-// const copy = numbers2.slice()
-// const deleted = copy.splice(5, 2)
-// console.log(copy)
-// console.log(deleted)
-// console.log(numbers2)
-
-// const numbers3 = [1, 2, 3, 6, 7, 8, 9]
-// const copy1 = numbers3.slice()
-// const deleted1 = copy1.splice(3, 0, 4, 5)
-// console.log(copy1)
-// console.log(deleted1)
-
-// const numbers4 = [1, 2, 3, 4, 4, 4, 7]
-// const copy2 = numbers4.slice()
-// const deleted2 = copy2.splice(4, 2, 5, 6)
-// console.log(copy2)
-// console.log(deleted2)
 
 
-// const a = [1, 2, 3];
-// const b = [5, 6, 7]
-// const c = 4
-    
-// const arr = a.concat(c, b, 'abc') // [1, 2, 3, 4, 5, 6, 7, 'abc']
-// console.log(arr)
+console.log(111)
+
+function bar() {
+    console.log("bar");
+    console.log(222)
+  }
+  
+  function baz() {
+    console.log("baz");
+    console.log(444)
+  }
+  
+  function foo() {
+    console.log("foo");
+    bar();
+    console.log(333)
+    baz();
+      console.log(555)
+      foo()
+  }
+  
+  foo();
+
+  console.log('!!!')
